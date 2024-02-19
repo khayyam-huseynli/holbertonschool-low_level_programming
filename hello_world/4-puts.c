0a9b34c2358191ff440b53502719f8a96fb3ff3e
Khayyam <9126@holbertonstudents.com>
2024-02-19 00:07:45 -0800

    betty style

---
+++ hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - This prints given string with puts function
+ *
+ * Return: returns (0) case of Success
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
