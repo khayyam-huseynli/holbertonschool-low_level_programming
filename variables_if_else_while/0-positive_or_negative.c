f58c8582acb147c0e21018204c401a4c1ae33b1c
Khayyam <9126@holbertonstudents.com>
2024-02-19 03:49:34 -0800

    worked on given code

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,15 +1,18 @@
-#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
  * main - The entry point
- * @n: a random number
  * Description: Prints whether the number n is positive or negative
  * Return: Always (0) when Successfull
  */
 
-
-int main(int n)
+int main(void)
 {
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 		printf("is positive");
 	else
