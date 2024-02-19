264f6312de8cf0f6949097649d159992715f3ac8
Khayyam <9126@holbertonstudents.com>
2024-02-19 00:13:51 -0800

    betty style, printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - This prints given string with printf function
+ *
+ * Return: returns (0) case of Success
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
