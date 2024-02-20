7582e50facc0354d1c5b6e20a7589654796fe3f0
Khayyam <9126@holbertonstudents.com>
2024-02-20 00:22:20 -0800

    prints the last digit of the number-edited

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -13,17 +13,19 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	int last_digit = n % 10;
-	
+	int last_digit;
+
+	last_digit = n % 10;
+
 	if (last_digit > 5)
 
-		printf("Last digit of %i is %i and is greater than 5\n", n, last_digit);
+		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
 	else
 		if (n == 0)
-			printf("Last digit of %i is %i and is 0\n", n, last_digit);
+			printf("Last digit of %d is %d and is 0\n", n, last_digit);
 		else
 			if (n < 6 && !(n == 0))
-				printf("Last digit of %i is %i and is less than 6
-						and not 0\n", n, last_digit);
+				printf("Last digit of %d is %d and is less than 6
+						and is not 0\n", n, last_digit);
 	return (0);
 }
