cf74b54757dca1aad33fc0abcbea5a96d5bb086c
khayyam <khayyam.huseynli@gmail.com>
2024-02-20 08:03:40 -0800

    while loop with i--

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - The entry point
+ * Description: Prints the lowercase alphabet in reverse,
+ *              followed by a new line
+ * Return: Always (0) when Successfull
+ */
+
+int main(void)
+{
+	char i = 'z';
+
+	while (i >= 'a')
+	{
+		putchar(i);
+		i--
+	}
+	putchar('\n');
+
+	return (0);
+}
