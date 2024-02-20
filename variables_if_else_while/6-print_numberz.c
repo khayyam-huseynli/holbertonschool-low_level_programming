e45e3aea007680adf43f2893c2ae2f72133b25f8
khayyam <khayyam.huseynli@gmail.com>
2024-02-20 07:40:56 -0800

    used for loop

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - The entry point
+ * Description: Prints all single digit numbers of base 10
+ *              starting from 0, followed by a new line
+ * Return: Always (0) when Successfull
+ */
+
+int main(void)
+{
+	for (int i = 0; i <= 9; i++)
+	{
+
+		putchar('0' + i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
