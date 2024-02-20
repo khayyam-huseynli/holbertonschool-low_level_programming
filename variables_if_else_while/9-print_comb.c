31b4dd1371ab090f79274577fe5e3833dbc676bc
khayyam <khayyam.huseynli@gmail.com>
2024-02-20 09:29:04 -0800

    all possible combinations of single-digit numbers

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - The entry point
+ * Description: Prints all possible combinations of 
+ *              single-digit numbers.
+ *              Numbers must be separated by ,, followed by a space
+ *              Numbers should be printed in ascending order
+ * Return: Always (0) when Successfull
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar('0' + i);
+
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
