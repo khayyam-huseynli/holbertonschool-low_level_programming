de4471773af8ac460dbec4a0bf2c6d47c8a61b60
khayyam <khayyam.huseynli@gmail.com>
2024-02-20 11:28:18 -0800

    nested for loop

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+/**
+ * main - The entry point
+ * Description: Prints all possible combinations of two digits
+ *              Numbers must be separated by ,, followed by a space
+ *              The two digits must be different
+ *              01 and 10 are considered the same combination of
+ *              the two digits 0 and 1
+ *              Print only the smallest combination of two digits
+ *              Numbers should be printed in ascending order,
+ *              with two digits
+ * Return: Always (0) when Successfull
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+
+		for (j = 0; j <= 9; j++)
+		{
+			if (i == j)
+			{
+			break;
+			}
+
+			putchar('0' + i);
+			putchar('0' + j);
+
+			if (i < 9)
+			{
+			putchar(',');
+			putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
