6bd2297460951f422c3a8de5fcc234750f5648e2
khayyam <khayyam.huseynli@gmail.com>
2024-02-21 00:06:17 -0800

    added missed ;, removed : after while

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -5,8 +5,9 @@ int main(void)
 	int i = 0;
 	char array[8] = {'_', 'p', 'u', 't',
 			'c', 'h', 'a', 'r'};
-	while (i < 8):
+	while (i < 8)
 	{
-		_putchar(array[i])
-		i++
+		_putchar(array[i]);
+		i++;
 	}
+}
