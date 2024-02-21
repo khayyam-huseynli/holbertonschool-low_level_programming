aac5d7c0cdad07b536d470daf522c5c5ee325bc7
khayyam <khayyam.huseynli@gmail.com>
2024-02-21 01:41:52 -0800

    addet prototype to main.h and writed function

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Function that prints 10 times the alphabet,
+ *                      in lowercase, followed by a new line
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i =  0, i < 10, i++)
+	{
+		char alphabet = 'a';
+
+		while (alphabet <= 'z')
+		{
+
+			_putchar(alphabet);
+
+			alphabet++;
+		}
+
+		_putchar('\n');
+	}
+}
