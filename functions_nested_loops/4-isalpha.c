65ec066b01c6eec5fbb0bd0c8ed3e5591811c0a2
khayyam <khayyam.huseynli@gmail.com>
2024-02-22 09:33:02 +0000

    added protitype to main.h+used for loop for less code

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -2,11 +2,8 @@
 
 /**
  * _isalpha - Function that checks for lowercase character
- *
  * @c: The character to be checked
- *
  * Return: returns 1 if character is lowercase, 0 otherwise
- *
  */
 
 int _isalpha(int c)
@@ -14,30 +11,26 @@ int _isalpha(int c)
 	int i;
 	int count = 0;
 
-	char lower_alphabet = 'a';
-	char upper_alphabet = 'A';
-
-	while (lower_alphabet <= 'z')
+	for (i = 'a'; i <= 'z'; i++)
 	{
+		if (c == i)
+		{
+			count++;
+			break;
+		}
 
-		_putchar(lower_alphabet);
-		lower_alphabet++;
-		count++;
-	}
-
-	while (upper_alphabet <= 'Z')
+	for (i = 'A'; i <= 'Z'; i++)
 	{
-
-		_putchar(upper_alphabet);
-		upper_alphabet++;
-		count++;
-	}
+		if (c == i)
+		{
+			count++;
+			break;
+		}
 
 	if (count > 0)
 	{
 		return (1);
 	}
-
 	else
 	{
 		return (0);
