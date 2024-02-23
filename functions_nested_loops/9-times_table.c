912b6fea93ae39a71fbdb6a3e3157ec6721224e8
khayyam <khayyam.huseynli@gmail.com>
2024-02-23 05:23:52 +0000

    used two for loop to multiple number 0-9, used / and % for printing two digit number with putchar

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * times_table - Function that prints the 9 times table,
+ *               starting with 0
+ */
+
+void times_table(void)
+{
+	int i;
+	int j;
+	int result;
+
+	for (i =  0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			result = i * j;
+
+			if (result >= 10)
+			{
+				_putchar(' ');
+				_putchar('0' + (result / 10));
+				_putchar('0' + (result % 10));
+				_putchar(',');
+			}
+			else if (result == 0)
+			{
+				_putchar('0' + result);
+				_putchar(',');
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + result);
+			}
+		}
+	}
+}
