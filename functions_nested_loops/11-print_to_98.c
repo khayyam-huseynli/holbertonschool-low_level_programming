d172a5a7e638b1cfa97578845b381fa56ac1aff7
khayyam <khayyam.huseynli@gmail.com>
2024-02-23 09:15:56 +0000

    combined < and = 98 cases, corrected for loops condition part, added extra printf for last output to be correct

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -10,26 +10,23 @@
 void print_to_98(int n)
 {
 
-	if (n < 98)
+	if (n <= 98)
 	{
-		for (; n <= 98; n++)
+		for (; n < 98; n++)
 		{
 			printf("%d, ", n);
 		}
 
-		_putchar('\n');
-	}
-	else if (n == 98)
-	{
 		printf("%d\n", n);
 	}
+
 	else
 	{
-		for (; n >= 98; n--)
+		for (; n > 98; n--)
 		{
 			printf("%d, ", n);
 		}
 
-		_putchar('\n');
+		printf("%d\n", n);
 	}
 }
