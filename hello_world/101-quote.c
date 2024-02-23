0e0aa5846786a2b4ee2b059deb1da8832a6c1c68
khayyam <khayyam.huseynli@gmail.com>
2024-02-23 09:42:54 +0000

    used sizeof insted of strlen

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -12,6 +12,6 @@ int main(void)
 {
 	char buf[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, buf, strnlen(buf, sizeof(buf)));
+	write(2, buf, sizeof(buf) - 1);
 	return (1);
 }
