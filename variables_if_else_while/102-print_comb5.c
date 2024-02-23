6d8a1aa6258cbf5bdd94e6023c75e78725b3e40e
khayyam <khayyam.huseynli@gmail.com>
2024-02-23 21:01:20 +0000

    added if inside if

--- variables_if_else_while/102-print_comb5.c
+++ variables_if_else_while/102-print_comb5.c
@@ -38,13 +38,12 @@ int main(void)
 						putchar(' ');
 						putchar('0' + m);
 						putchar('0' + n);
-					}
 
-					if (i != 9 || j != 8 || m != 9 || n != 9)
-					{
-
-						putchar(',');
-						putchar(' ');
+						if (i != 9 || j != 8 || m != 9 || n != 9)
+						{
+							putchar(',');
+							putchar(' ');
+						}
 					}
 				}
 			}
