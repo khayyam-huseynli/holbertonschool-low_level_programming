c0cd8c48cceb24f3aa1690590fbe93dc1066b44a
khayyam <khayyam.huseynli@gmail.com>
2024-02-23 08:32:23 +0000

    removed i=n out of for loop

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -10,10 +10,11 @@
 void print_to_98(int n)
 {
 	int i;
+	i = n;
 
 	if (n < 98)
 	{
-		for (i = n; i <= 98; i++)
+		for (; i <= 98; i++)
 		{
 			printf("%d, ", i);
 		}
@@ -26,7 +27,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-		for (i = n; i <= 98; i--)
+		for (; i <= 98; i--)
 		{
 			printf("%d, ", i);
 		}
