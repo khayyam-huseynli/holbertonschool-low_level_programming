3f3489fe25f847e58dd614a4fc2622d1839097d4
khayyam <khayyam.huseynli@gmail.com>
2024-02-23 07:58:13 +0000

    minor bugs fixed

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -13,14 +13,14 @@ void print_to_98(int n)
 
 	if (n < 98)
 	{
-		for (i = 1; i <= 98; i++)
+		for (i = n; i <= 98; i++)
 		{
 			printf("%d, ", i);
 		}
 	}
 	else if (n == 98)
 	{
-		printf("%d", n);
+		printf("%d ", n);
 	}
 	else
 	{
