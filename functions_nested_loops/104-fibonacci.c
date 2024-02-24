734f4d5674d868ef6c9467e8d0dda8b535c182cc
khayyam <khayyam.huseynli@gmail.com>
2024-02-24 18:32:56 +0000

    used 102-fibonacci.c

---
+++ functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Returns 0 when successfull
+ */
+
+int main(void)
+{
+	int i;
+	long int f1 = 1;
+	long int f2 = 2;
+	long int fnext = f1 + f2;
+
+	printf("%ld, %ld, ", f1, f2);
+
+	for (i = 3; i <= 98; i++)
+	{
+
+		printf("%ld", fnext);
+
+		if (i < 98)
+		{
+			printf(", ");
+		}
+		else
+		{
+			printf("\n");
+		}
+
+		f1 = f2;
+		f2 = fnext;
+		fnext = f1 + f2;
+	}
+	return (0);
+}
