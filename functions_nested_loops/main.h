5361111b4ae2a0a1f348f5692a12f7184e7aefc0
khayyam <khayyam.huseynli@gmail.com>
2024-02-24 19:00:18 +0000

    nested loop

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -9,6 +9,7 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
+void print_times_table(int n);
 void print_to_98(int n);
 void fibonacci(int n);
 void natural(int n);
