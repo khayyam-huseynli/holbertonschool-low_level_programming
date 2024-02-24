be11bb2e9c9ce94dd5ce4106156f809a8df0d449
khayyam <khayyam.huseynli@gmail.com>
2024-02-24 13:58:48 +0000

    added main function

--- functions_nested_loops/101-natural.c
+++ functions_nested_loops/101-natural.c
@@ -2,6 +2,24 @@
 #include <stdio.h>
 
 /**
+ * main - Entry point
+ * return: Returns 0 always
+ */
+
+/**
+* @n: nuber to be checked
+*/
+
+void natural(int n);
+
+int main(void)
+{
+	natural(1024);
+
+	return (0);
+}
+
+/**
  * natural - Function that computes and prints the sum of all the
  *           multiples of 3 or 5 below 1024 (excluded), followed by a new line.
  * @n: nuber to be checked
