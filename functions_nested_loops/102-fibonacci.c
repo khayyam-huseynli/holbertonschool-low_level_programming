a922c8e42ae0fe9b483462c630ed7715e9bd0594
khayyam <khayyam.huseynli@gmail.com>
2024-02-24 12:54:27 +0000

    prints 50 elements of fibonacci

---
+++ functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * fibonacci - Function that prints  the first 50 Fibonacci numbers,
+ *		       starting with 1 and 2, followed by a new line.
+ * @n: nuber to be added
+ */
+
+void fibonacci(int n)
+{
+	int i;
+	char fibbo[];
+
+	fibbo[0] = 1;
+	fibbo[1] = 2;
+
+	for (i = 0; i < n - 2; i++)
+	{
+		fibbo[i + 2] = fibbo[i + 1] + fibbo[i];
+		printf("%d, ", fibbo[i]);
+	}
+}
