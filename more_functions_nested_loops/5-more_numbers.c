cd2629af81cc4949551618dcb325563104459387
khayyam <khayyam.huseynli@gmail.com>
2024-02-26 07:36:36 +0000

    for inside while inside if

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * more_numbers - function that prints 10 times the numbers,
+ *                from 0 to 14, followed by a new line
+ */
+
+void more_numbers(void)
+{
+	int d = 0;
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+
+		while (d <= 14)
+		{
+			if (d > 9)
+			{
+				_putchar ('0' + d / 10);
+			}
+
+			_putchar('0' + d % 10);
+			d++;
+		}
+		_putchar('\n');
+	}
+}
