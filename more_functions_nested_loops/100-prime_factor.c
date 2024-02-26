1794e2b65c229892b3fd19cec8250e50c49c38f3
khayyam <khayyam.huseynli@gmail.com>
2024-02-26 21:41:15 +0400

    used for loop, odd nums

---
+++ more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * main -   Function that finds and prints the largest prime
+ *          factor of the number 612852475143, followed by a new line.
+ * Return: always returns 0 when successfull
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int i = 3;
+	int n = 612852475143;
+
+	while (n % 2 == 0)
+	{
+		n = n / 2;
+	}
+
+	for (i = 3; i * i <= n; i = i + 2)
+	{
+		while (n % i == 0)
+		{
+			n = n / i;
+		}
+	}
+
+	printf("%d\n", n);
+
+	return (0);
+}
