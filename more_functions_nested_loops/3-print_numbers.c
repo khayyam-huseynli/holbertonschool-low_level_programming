52b3f6aefa39186f4439ce89b0c2a2a348184995
khayyam <khayyam.huseynli@gmail.com>
2024-02-26 06:48:55 +0000

    prints numbers 0-9

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that prints the numbers, from 0 to 9,
+ *                 followed by a new line
+ */
+
+void print_numbers(void)
+{
+	int d;
+
+	while (d >= 0 && d <= 9)
+	{
+		_putchar ('0' + d);
+	}
+}
