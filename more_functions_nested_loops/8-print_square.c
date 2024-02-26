6fcd3f8743859bcf97e7d48d21ab22a56ece08dd
khayyam <khayyam.huseynli@gmail.com>
2024-02-26 10:10:58 +0000

    nested for loop, i<n, j<n

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_square  -   Function that prints a square, followed by a new line
+ *                   The diogonal should end with a \n
+ *                   If n is 0 or less, the function should only print \n
+ *                   If size is 0 or less, the function should print only a new line
+ *                   Use the character # to print the square
+ * @n: is the is the size of the square
+ */
+
+void print_square(int size)
+{
+	int i = 0;
+	int j = 0;
+
+	if (n <=  0)
+	{
+		_putchar ('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
