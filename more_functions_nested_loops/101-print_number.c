49889d0383491cf9751d64f8a62c7092212735c9
khayyam <khayyam.huseynli@gmail.com>
2024-02-27 06:12:09 +0000

    corrected for negative long numbers

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -11,12 +11,19 @@ void print_number(int n)
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
 	}
 
 	if (n / 10)
 	{
 		print_number(n / 10);
 	}
+
+	if (n < 0)
+	{
+		_putchar(-1 * (n % 10) + '0');
+	}
+	else
+	{
 		_putchar(n % 10 + '0');
+	}
 }
