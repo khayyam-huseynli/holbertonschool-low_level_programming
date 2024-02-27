9a2b99ae136e772ae0c077df0a18aa492f5d421b
khayyam <khayyam.huseynli@gmail.com>
2024-02-27 09:00:06 +0000

    used sizeof(*s)

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strlen - function that returns the length of a string
+ * @s: string to be mesured
+ * Return: returns size of given string
+ */
+
+int _strlen(char *s)
+{
+	return (sizeof(*s));
+}
