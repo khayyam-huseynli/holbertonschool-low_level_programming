03351264b788f83eb209058158c430c0c93d1109
khayyam <khayyam.huseynli@gmail.com>
2024-02-27 23:07:23 +0400

    added 2 while, 1 for length of string, 1 for reverse printing

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_rev - function that prints a string, in reverse,
+ *             followed by a new line.
+ * @s: string to be reversed
+ */
+
+void print_rev(char *s)
+{
+
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	int j = i;
+
+	while (s[j] != '\0')
+	{
+		_putchar(s[i]);
+		i--;
+	}
+
+	_putchar('\n');
+}
