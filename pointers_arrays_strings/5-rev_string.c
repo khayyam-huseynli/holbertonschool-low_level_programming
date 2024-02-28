e47d8c38ce9c5f369a27d203a4c401c4ea8db9e4
khayyam <khayyam.huseynli@gmail.com>
2024-02-28 13:56:32 +0400

    fixed: char *start = *s to char *start = s

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -8,24 +8,24 @@
 void rev_string(char *s)
 {
 
-        int i = 0;
+	int i = 0;
 
-        while (s[i] != '\0')
-        {
-                i++;
-        }
+	while (s[i] != '\0')
+	{
+		i++;
+	}
 
-        /** after while loop i = strlen + 1 */
+	/** after while loop i = strlen + 1 */
 
-        char *start = *s; /* points 1st character */
-        char *end = *s + i - 2; /* points last character */
+	char *start = s; /* points 1st character */
+	char *end = s + i - 2; /* points last character */
 
-        while (start < end)
-        {
-                char temp = *start;
-                *start = *end;
-                *end = temp;
-                start++;
-                end--;
-        }
+	while (start < end)
+	{
+		char temp = *start;
+		*start = *end;
+		*end = temp;
+		start++;
+		end--;
+	}
 }
