c8d511f13a891bb12eb01b7df569dd2d88a80c32
khayyam <khayyam.huseynli@gmail.com>
2024-02-28 14:03:10 +0400

    moved all declarations before while statements

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -9,6 +9,8 @@ void rev_string(char *s)
 {
 
 	int i = 0;
+	char *start = s; /* points 1st character */
+	char *end = s + i - 2; /* points last character */
 
 	while (s[i] != '\0')
 	{
@@ -17,8 +19,6 @@ void rev_string(char *s)
 
 	/** after while loop i = strlen + 1 */
 
-	char *start = s; /* points 1st character */
-	char *end = s + i - 2; /* points last character */
 
 	while (start < end)
 	{
