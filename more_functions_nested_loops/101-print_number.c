373c49aa07b025aa79cb0f19dc4a046559a0a9fa
khayyam <khayyam.huseynli@gmail.com>
2024-02-29 23:00:27 +0400

    used recursion, unsigned int and correction for negative nums

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -5,25 +5,18 @@
  * @n: number to be printed
  */
 
-
 void print_number(int n)
 {
+	unsigned int num = n;
+
 	if (n < 0)
 	{
 		_putchar('-');
+		num = -num;
 	}
 
-	if (n / 10)
-	{
-		print_number(n / 10);
-	}
+	if ((num / 10) > 0)
+		print_number(num / 10);
 
-	if (n < 0)
-	{
-		_putchar(-1 * (n % 10) + '0');
-	}
-	else
-	{
-		_putchar(n % 10 + '0');
-	}
+	_putchar((num % 10) + '0');
 }
