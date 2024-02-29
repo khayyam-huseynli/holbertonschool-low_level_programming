c30d1b641383e1999017a96c8b1c5f2b990f1982
khayyam <khayyam.huseynli@gmail.com>
2024-02-29 21:29:24 +0400

    used while loop and sign parameter

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -13,15 +13,22 @@ int _atoi(char *s)
 {
 
 	int i = 0;
+	int sign = 1;
 	int res = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (s[i] < '0' || s[i] > '9')
 	{
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			res = res * 10 + s[i] - '0';
-		}
+		i++;
+	}
+	if (s[i - 1] == '-')
+	{
+		sign = -1;
 	}
 
-	return (res);
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		res = 10 * res + (s[i++] - '0');
+	}
+
+	return (res * sign);
 }
