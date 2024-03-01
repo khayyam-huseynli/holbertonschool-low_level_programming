c4032815b6dd2ff9488387ecfde13aac63d832c8
khayyam <khayyam.huseynli@gmail.com>
2024-03-01 13:11:31 +0400

    added if when n>lengthofsrc

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -15,11 +15,22 @@ char *_strncpy(char *dest, char *src, int n)
 
 	int i = 0;
 	int j = 0;
+	int l = 0;
 
 	while (i < n)
 	{
 		dest[i] = '\0';
 		i++;
+	}
+
+	while (src[l] != '\0')
+	{
+		l++;
+	}
+
+	if (n > l)
+	{
+		n = l;
 	}
 
 	while (j < n)
