71f1669ad9d6fe47a9d1eba1af0abd9fa3803c4f
khayyam <khayyam.huseynli@gmail.com>
2024-03-01 05:57:03 +0400

    first try

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * _strcat - function that concatenates two strings,
+ *           This function appends the src string to
+ *           the dest string, overwriting the terminating
+ *           null byte (\0) at the end of dest, and
+ *           then adds a terminating null byte
+ * @src: string to be concatenated
+ * @dest: destination point in buffer
+ *
+ *Return: retuns pointer of destination
+ */
+
+char *_strcat(char *dest, char *src)
+{
+
+	int i = 0;
+	int j = 0;
+	int k = 0;
+
+	while (src[i] != '\0')
+	{
+		i++;
+	}
+
+	/** after while loop i = strlen(src) */
+
+	while (dest[j] != '\0')
+	{
+		j++;
+	}
+
+	/** after while loop j = strlen(dest) */
+
+	for (k = 0; k <= i; k++)
+	{
+		dest[j + 1 + k] = src[k];
+	}
+
+	/*ptr_dest = dest;*/
+
+	return (dest);
+}
