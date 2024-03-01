3d5d9daeae870dca2c3df06461d0aa5593a79337
khayyam <khayyam.huseynli@gmail.com>
2024-03-01 04:11:26 +0000

    similar to 0-strcat

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strncat - function that concatenates two strings,
+ *            This function appends at most n bytes from src to
+ *            the dest string, overwriting the terminating
+ *            null byte (\0) at the end of dest, and
+ *            then adds a terminating null byte
+ * @src: string to be concatenated
+ * @dest: destination point in buffer
+ * @n: number of characters to be concatenated
+ *Return: retuns pointer of destination
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+
+	int j = 0;
+	int k = 0;
+
+	while (dest[j] != '\0')
+	{
+		j++;
+	}
+
+	/** after while loop j = strlen(dest) */
+
+	for (k = 0; k <= n; k++)
+	{
+		dest[j + k] = src[k];
+	}
+
+	return (dest);
+}
