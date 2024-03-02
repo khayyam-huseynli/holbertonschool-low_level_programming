c4fef6a27d2c2d69c7384e30bc23b8bed350b5c8
khayyam <khayyam.huseynli@gmail.com>
2024-03-02 16:58:50 +0400

    -13 if >n,N +13 if <n,N

---
+++ pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * rot13 - Function that encodes a string using rot13.
+ *
+ * @str: string to be encoded
+ *
+ * Return: retuns pointer of string
+ */
+
+char *rot13(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if ((str[i] >= 'a' && str[i] <= 'z' || str[i] >= 'A' 
+			&& str[i] <= 'Z') && (str[i] < 'n' || str[i] < 'N'))
+		{
+			str[i] = str[i] + 13;
+		}
+		else
+		{
+			str[i] = str[i] - 13;
+		}
+
+		i++;
+	}
+
+	return (str);
+}
