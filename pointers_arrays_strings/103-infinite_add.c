1283d485cbc5fd19f6dc3975b9525df0729fb182
khayyam <khayyam.huseynli@gmail.com>
2024-03-03 16:28:02 +0400

    setted k+2>size_r

--- pointers_arrays_strings/103-infinite_add.c
+++ pointers_arrays_strings/103-infinite_add.c
@@ -32,7 +32,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		r[k] = sum % 10 + '0';
 		sum = (sum - sum % 10) / 10;
 		k++;
-		if (k + 1 > size_r)
+		if (k + 2 > size_r)
 		{
 			return (0);
 		}
