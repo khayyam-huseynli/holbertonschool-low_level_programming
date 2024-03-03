5e024dcd3997191b550a24a92415419eeb83eddd
khayyam <khayyam.huseynli@gmail.com>
2024-03-03 20:11:44 +0400

    changed approach

--- pointers_arrays_strings/103-infinite_add.c
+++ pointers_arrays_strings/103-infinite_add.c
@@ -1,7 +1,7 @@
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int l1, l2, tmpl, rl, i, sum, num1, num2, carry;
-	char tmp[10000], *start, *end;
+	int l1, l2, rl, i, sum, num1, num2, carry;
+	char *start, *end;
 
 	rl = i = l1 = l2 = sum = num1 = num2 = carry = 0;
 	while (n1[l1] != '\0')
