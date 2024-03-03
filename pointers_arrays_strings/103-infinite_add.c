9cf516caf91fc4cf1adbeb11980f3a695d7b8b71
khayyam <khayyam.huseynli@gmail.com>
2024-03-03 20:05:25 +0400

    changed approach

--- pointers_arrays_strings/103-infinite_add.c
+++ pointers_arrays_strings/103-infinite_add.c
@@ -1,47 +1,57 @@
-#include "main.h"
-
-/**
- * infinite_add - Function that adds two integers and returns the result
- * @n1: nuber in string to be added
- * @n2: number in string to be added
- * @r: is the buffer that the function will use to store the result
- * @size_r: size of buffer
- * Return: Always returns a pointer to the result
- */
-
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int num1 = 0, num2 = 0, sum = 0;
-	int i = 0, j = 0, k = 0, m = 0;
-	char temp_r[1000];
+	int l1, l2, tmpl, rl, i, sum, num1, num2, carry;
+	char tmp[10000];
 
-	while (n1[i] != '\0')
+	rl = i = l1 = l2 = sum = num1 = num2 = carry = 0;
+	while (n1[l1] != '\0')
+		l1++;
+	while (n2[l2] != '\0')
+		l2++;
+	if (l1 + 2 > size_r || l2 + 2 > size_r)
+		return (0);
+	l1--;
+	l2--;
+	while (i <= l1 || i <= l2)
 	{
-		num1 = num1 * 10 + n1[i] - '0';
+		num1 = num2 = 0;
+		if (i <= l1)
+			num1 = n1[l1 - i] - '0';
+		if (i <= l2 && (l2 - i) >= 0)
+			num2 = n2[l2 - i] - '0';
+		sum = num1 + num2 + carry;
+		if (sum >= 10)
+		{
+			carry = 1;
+			sum -= 10;
+		}
+		else
+			carry = 0;
+		r[i] = sum + '0';
+		i++;
+		rl++;
+	}
+	if (carry > 0)
+	{
+		r[i] = carry + '0';
+		r[i + 1] = '\0';
+	}
+	i = tmpl = 0;
+	while (i <= rl)
+	{
+		tmp[i] = r[rl - i];
+		tmpl++;
 		i++;
 	}
-	while (n2[j] != '\0')
+	i = 0;
+	while (i < tmpl)
 	{
-		num2 = num2 * 10 + n2[j] - '0';
-		j++;
-	}
-
-	if (i + 2 > size_r || j + 2 > size_r)
-		return (0);
-	sum = num1 + num2;
-
-	while (k < i || k < j)
-	{
-		temp_r[k] = sum % 10 + '0';
-		sum = (sum - sum % 10) / 10;
-		k++;
-	}
-	temp_r[k] = '\0';
-
-	while (temp_r[m] != '\0')
-	{
-		r[m] = temp_r[k - m - 1];
-		m++;
+		if (r[i] == '\0')
+		{
+			break;
+		}
+		r[i] = tmp[i];
+		i++;
 	}
 	return (r);
 }
