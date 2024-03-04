304c8a785c416dc6a77f13d440d9e066aa0abde3
khayyam <khayyam.huseynli@gmail.com>
2024-03-04 16:01:54 +0400

    used copynstring func

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * _memcpy - function that copies copies memory area 
+ *           from src,to memory area pointed to by dest
+ * @src: memory area to be copied
+ * @dest: destination memory area
+ * @n: number of bytes to be copied
+ *
+ *Return: retuns pointer of destination
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+
+	int i = 0;
+	int j = 0;
+	int l = 0;
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	while (src[l] != '\0')
+	{
+		l++;
+	}
+
+	if (n > l)
+	{
+		n = l;
+	}
+
+	while (j < n)
+	{
+		if (src[0] == '\0')
+		{
+			break;
+		}
+
+		*(dest + j) = *(src + j);
+		j++;
+	}
+
+	return (dest);
+}
