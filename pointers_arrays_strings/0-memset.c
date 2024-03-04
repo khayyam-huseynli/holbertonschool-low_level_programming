47195c03ea7138464ac604f0311c1e8970d32c7d
khayyam <khayyam.huseynli@gmail.com>
2024-03-04 15:24:02 +0400

    used 1 for loop

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _memset - function that fills memory with a constant byte.
+ *            This function function fills the first n bytes of the
+ *            memory area pointed to by s with the constant byte b
+ * @s: memory area to be filled
+ * @b: bytes to be filled to memory
+ * @n: number of characters to be filled
+ *Return: retuns pointer to memory
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+
+	int i = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+		if (src[i] == '\0')
+		{
+			break;
+		}
+	}
+
+	return (s);
+}
