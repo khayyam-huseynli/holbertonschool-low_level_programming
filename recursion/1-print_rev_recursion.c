aca994abbe2ac1a68283ffbde65a8d425f4468ca
khayyam <khayyam.huseynli@gmail.com>
2024-03-06 09:04:55 +0000

    used *(s+1)==\0 in if, placed putchar after recursion

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion -  a function that prints a string,
+ *                         in reverse
+ * @s: string to be printed
+ *
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*(s + 1) == '\0')
+	{
+		return;
+	}
+	_print_rev_recursion(++s);
+	_putchar(*s);
+}
