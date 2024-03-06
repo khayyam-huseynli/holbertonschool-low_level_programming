5140d9cbc6b2432ad941f0a712f73844434b5e60
khayyam <khayyam.huseynli@gmail.com>
2024-03-07 01:34:45 +0400

    a is given flattened(1d) 2d array

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -6,28 +6,18 @@
  *                  the two diagonals of a square
  *                  matrix of integers
  *
- * @a:  is the 2d array to be calculated
+ * @a:  is the flattened(1d) 2d array to be calculated
  * @size: size of given 2d square matrix
  */
 
 void print_diagsums(int *a, int size)
 {
-	int i = 0, j = 0;
-	int d1 = 0, d2 = 0;
+	int i, n, sum1 = 0, sum2 = 0;
 
-	for (i = 0; i < size; i++)
-	{
-		for (j = 0; j < size; j++)
-		{
-			if (i == j)
-			{
-				d1 += a[i][j];
-			}
-			else if (i + j == size - 1)
-			{
-				d2 += a[i][j];
-			}
-		}
-		printf("%d, %d\n", d1, d2);
-	}
+	for (i = 0; i <= (size * size); i = i + size + 1)
+		sum1 = sum1 + a[i];
+
+	for (n = size - 1; n <= (size * size) - size; n = n + size - 1)
+		sum2 = sum2 + a[n];
+	printf("%d, %d\n", sum1, sum2);
 }
