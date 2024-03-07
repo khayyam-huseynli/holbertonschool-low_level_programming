f30b743fbcb515b86e89f77d75fca20ecc6ed0dc
khayyam <khayyam.huseynli@gmail.com>
2024-03-07 18:00:33 +0400

    removed argc and added returns  int main

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,11 +2,12 @@
 
 /**
 * main - prints programe name
-* @argc: number of cl arguments
 * @argv: array of all cl arguments
+* Return: always 0 when successfull
 */
 
-void main(int argc, char *argv[])
+int main(char *argv[])
 {
 	printf("%s\n", argv[0]);
+	return (0);
 }
