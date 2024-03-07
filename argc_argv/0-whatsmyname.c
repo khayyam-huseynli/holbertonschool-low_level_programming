698bda9a56a9fd27732b4697ad3029d2f6668181
khayyam <khayyam.huseynli@gmail.com>
2024-03-07 18:29:59 +0400

    added attribut unused for gcc, casting to void, (void)argc

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -7,8 +7,9 @@
 * Return: always 0 when successfull
 */
 
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n", argv[argc - 1]);
+	(void)argc;
+	printf("%s\n", argv[0]);
 	return (0);
 }
