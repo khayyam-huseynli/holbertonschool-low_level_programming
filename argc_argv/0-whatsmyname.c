652c9c10937c5efe8caa40b5038d99ef6ea3b098
khayyam <khayyam.huseynli@gmail.com>
2024-03-07 17:57:33 +0400

    used argc and argv in main

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - prints programe name
+* @argc: number of cl arguments
+* @argv: array of all cl arguments
+*/
+
+void main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+}
