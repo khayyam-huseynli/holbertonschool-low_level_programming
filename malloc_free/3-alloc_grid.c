18d1d07b86c0a885833544d31fcaab54f702892c
khayyam <khayyam.huseynli@gmail.com>
2024-03-12 01:03:52 +0400

    1st created array of pointers length of height, then arrays for each pointer length of width

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+*alloc_grid - function that returns a pointer to a
+*             2 dimensional array of integers
+*
+*@width:  width of 2d array
+*@height: height of 2d array
+*
+*Return: returns pointer to a 2d array of integers,
+*        NULL on failure or If width or height
+*        is 0 or negative
+*/
+
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **grid;
+
+	if (widh <= 0 || height <= 0)
+		return (NULL);
+
+	grid = malloc(sizeof(int *) * height);
+
+	for (i = 0; i < height; i++)
+		grid[i] = malloc(sizeof(int) * width);
+
+	if (grid == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		for (i = 0; i < width; i++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+
+	return (grid);
+}
