9bfb11932d354b1675361233b4d4ddab706602d9
khayyam <khayyam.huseynli@gmail.com>
2024-03-12 01:05:56 +0400

    minor bug fixed

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -19,7 +19,7 @@ int **alloc_grid(int width, int height)
 	int i, j;
 	int **grid;
 
-	if (widh <= 0 || height <= 0)
+	if (width <= 0 || height <= 0)
 		return (NULL);
 
 	grid = malloc(sizeof(int *) * height);
