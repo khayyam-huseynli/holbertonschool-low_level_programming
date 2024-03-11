d86e2887971e7619615a9b4ca42cded53c9c0d01
khayyam <khayyam.huseynli@gmail.com>
2024-03-11 21:18:21 +0400

    used length of str

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+*_strdup - function that returns a pointer to a newly
+*          allocated space in memory, which contains a
+*          copy of the string given as a parameter.
+*
+*@str: string to be duplicated
+*
+*Return: On success, returns a pointer to the duplicated string.
+*        returns NULL if insufficient memory was available or
+*        str = NULL
+*/
+
+char *_strdup(char *str)
+{
+	int i = 0, j = 0;
+	char *dup_str;
+
+	while (*str != '\0')
+	{
+		i++;
+	}
+
+	dup_str = malloc(sizeof(char) * i);
+
+	if (dup_str == NULL || *str == '\0')
+		return (NULL);
+
+	while (j < i)
+	{
+		*dup_str[j] =  *str[j];
+		j++;
+	}
+	return (dup_str);
+}
