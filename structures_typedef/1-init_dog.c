9922d60e8e056d249f0bbe794a8c0b0b937104c9
khayyam <khayyam.huseynli@gmail.com>
2024-03-13 09:47:04 +0000

    used malloc to create memory

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+
+/**
+*init_dog -  function that initialize a variable of type struct dog
+*
+*@d: struct dog
+*@name: name of dog
+*@age: age of dog
+*@owner: owner of dog
+*
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d = malloc(sizeof(struct d));
+	if (d == NULL)
+		return (NULL);
+	d - > name = *name;
+	d - > age = age;
+	d - > owner = *owner;
+}
