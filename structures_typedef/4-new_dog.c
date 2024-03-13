cb33d42873a81207fa4874e2b0e161526d476563
khayyam <khayyam.huseynli@gmail.com>
2024-03-14 02:32:36 +0400

    created another memory for duplication of strings

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,31 +1,51 @@
-#include "dog.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>
-
+#include "dog.h"
 /**
-*new_dog -  function that creates a new dog.
+* new_dog - creates a new dog
+* @name: name of dog
+* @age: age of dog
+* @owner: owner of dog
 *
-*@name: name of dog
-*@age: age of dog
-*@owner: owner of dog
-*
-*Return: returns pointer to new struct dog
-*        if function fails NULL
+* Return: pointer to new dog
 */
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *d;
+	unsigned int nl, ol, i;
+	dog_t *dog;
 
-	d = malloc(sizeof(dog_t));
-
-	if (d == NULL)
+	if (name == NULL || owner == NULL)
 		return (NULL);
 
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	dog = malloc(sizeof(dog_t));
 
-	return (d);
+	if (dog == NULL)
+		return (NULL);
+	for (nl = 0; name[nl]; nl++)
+		nl++;
+	dog->name = malloc(nl * sizeof(char));
+
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	for (i = 0; i < nl; i++)
+		dog->name[i] = name[i];
+
+	dog->age = age;
+
+	for (ol = 0; owner[ol]; ol++)
+		ol++;
+	dog->owner = malloc(ol * sizeof(char));
+
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	for (i = 0; i < ol; i++)
+		dog->owner[i] = owner[i];
+
+	return (dog);
 }
