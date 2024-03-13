d16a8afacecde4c8aca7583547ea512f14481e5d
khayyam <khayyam.huseynli@gmail.com>
2024-03-13 09:56:14 +0000

    deleted malloc

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -14,10 +14,7 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d = malloc(sizeof(struct d));
-	if (d == NULL)
-		return (NULL);
-	d - > name = *name;
-	d - > age = age;
-	d - > owner = *owner;
+	d->name = *name;
+	d->age = age;
+	d->owner = *owner;
 }
