59b315ab98e25c196736866467a95439f5d0a3bc
khayyam <khayyam.huseynli@gmail.com>
2024-03-13 08:33:31 +0000

    added include guard and description

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - creates dog struct
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner
+ *
+ * Description: struct data type
+ */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+
+#endif
