be85c44269da2633c82da743926a488eb8be397f
khayyam <khayyam.huseynli@gmail.com>
2024-03-14 00:44:47 +0400

    testing output for d=null

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -16,9 +16,18 @@
 
 void print_dog(struct dog *d)
 {
-	if (d)
+	if (d == NULL)
 	{
-		printf("Name: %s\nAge: %f\nOwner: %s\n",
-			d->name, d->age, d->owner);
+		if (d->name != NULL)
+			printf("Name: %s\n", d->name);
+		else
+			printf("Name: (nil)\n");
+
+		printf("Age: %.6f\n", d->age);
+
+		if (d->owner != NULL)
+			printf("Owner: %s\n", d->owner);
+		else
+			printf("Owner: (nil)\n");
 	}
 }
