15ed85e195834fd2e3738f1ff4203aceb7d0696c
khayyam <khayyam.huseynli@gmail.com>
2024-03-14 00:23:26 +0400

    used if(d) and printf

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+*print_dog - function that prints a struct dog
+*            if an element of d is NULL, print (nil)
+*            instead of this element.
+*            (if name is NULL, print Name: (nil))
+*            if d is NULL print nothing.
+*
+*@d: struct dog
+*
+*/
+
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		printf("Name: %s\n Age: %f\n Owner: %s\n",
+			d->name, d->age, d->owner);
+	}
+}
