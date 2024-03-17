27668a19ad201c9f6c4b8683429415329733ac4d
khayyam <khayyam.huseynli@gmail.com>
2024-03-17 22:55:45 +0400

    changed entire code

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -1,19 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Function prototypes */
-int is_digit(char c);
-int str_len(char *s);
-void print_error_and_exit(char *result);
-char *multiply_large_numbers(char *num1, char *num2);
-
 /* Function to check if a character is a digit */
 int is_digit(char c) {
     return c >= '0' && c <= '9';
 }
 
 /* Function to calculate the length of a string */
-int str_len(char *s) {
+int str_len(const char *s) {
     int length = 0;
     while (s[length] != '\0') {
         length++;
@@ -22,61 +16,70 @@ int str_len(char *s) {
 }
 
 /* Function to print an error message and exit */
-void print_error_and_exit(char *result) {
-    if (result != NULL) {
-        free(result);
-    }
+void print_error_and_exit() {
     printf("Error\n");
     exit(98);
 }
 
 /* Function to multiply two large numbers */
-char *multiply_large_numbers(char *num1, char *num2) {
-    int len1, len2, i, j, carry, sum, start, new_length;
+char *multiply_large_numbers(const char *num1, const char *num2) {
+    int len1 = str_len(num1);
+    int len2 = str_len(num2);
+    int i, j, carry, sum;
     char *result;
 
-    len1 = str_len(num1);
-    len2 = str_len(num2);
-    result = malloc(len1 + len2);
+    if (len1 == 0 || len2 == 0) {
+        return NULL;
+    }
+
+    result = malloc(len1 + len2 + 1);
     if (result == NULL) {
-        print_error_and_exit(result);
+        print_error_and_exit();
     }
 
     for (i = 0; i < len1 + len2; i++) {
-        result[i] = 0;
+        result[i] = '0';
     }
+    result[len1 + len2] = '\0';
 
     for (i = len1 - 1; i >= 0; i--) {
         if (!is_digit(num1[i])) {
-            print_error_and_exit(result);
+            free(result);
+            print_error_and_exit();
         }
         carry = 0;
         for (j = len2 - 1; j >= 0; j--) {
             if (!is_digit(num2[j])) {
-                print_error_and_exit(result);
+                free(result);
+                print_error_and_exit();
             }
-            sum = (num1[i] - '0') * (num2[j] - '0') + result[i + j + 1] + carry;
+            sum = (num1[i] - '0') * (num2[j] - '0') + (result[i + j + 1] - '0') + carry;
             carry = sum / 10;
-            result[i + j + 1] = sum % 10;
+            result[i + j + 1] = (sum % 10) + '0';
         }
         if (carry > 0) {
-            result[i + len2] += carry;
+            result[i + j + 1] = (result[i + j + 1] - '0' + carry) + '0';
         }
     }
 
-    start = 0;
-    while (start < len1 + len2 - 1 && result[start] == 0) {
-        start++;
+    /* Remove leading zeros */
+    for (i = 0; i < len1 + len2 - 1; i++) {
+        if (result[i] != '0') {
+            break;
+        }
     }
-
-    if (start != 0) {
-        new_length = len1 + len2 - start;
-        for (i = 0; i < new_length; i++) {
-            result[i] = result[i + start];
+    if (i > 0) {
+        char *trimmed_result = malloc(len1 + len2 - i + 1);
+        if (trimmed_result == NULL) {
+            free(result);
+            print_error_and_exit();
         }
-        for (; i < len1 + len2; i++) {
-            result[i] = 0;
+        for (j = 0; i < len1 + len2; j++, i++) {
+            trimmed_result[j] = result[i];
         }
+        trimmed_result[j] = '\0';
+        free(result);
+        return trimmed_result;
     }
 
     return result;
@@ -84,28 +87,19 @@ char *multiply_large_numbers(char *num1, char *num2) {
 
 /* Main function */
 int main(int argc, char *argv[]) {
+    
     char *result;
-    int result_length, start, i;
 
     if (argc != 3) {
-        print_error_and_exit(NULL);
+        print_error_and_exit();
     }
 
     result = multiply_large_numbers(argv[1], argv[2]);
     if (result == NULL) {
-        print_error_and_exit(NULL);
+        print_error_and_exit();
     }
 
-    result_length = str_len(argv[1]) + str_len(argv[2]);
-    start = 0;
-    while (start < result_length - 1 && result[start] == 0) {
-        start++;
-    }
-    for (i = start; i < result_length; i++) {
-        printf("%d", result[i]);
-    }
-    printf("\n");
-
+    printf("%s\n", result);
     free(result);
     return 0;
 }
