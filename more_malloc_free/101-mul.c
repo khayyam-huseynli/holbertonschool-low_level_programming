40b3125d8144485f6c5052fbe9deeef0bad52f3b
khayyam <khayyam.huseynli@gmail.com>
2024-03-17 23:06:44 +0400

    final file

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -1,106 +1,99 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
 
-/* Function to check if a character is a digit */
-int is_digit(char c) {
-    return c >= '0' && c <= '9';
+/**
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
+ *
+ * Return: 0 if a non-digit is found, 1 otherwise
+ */
+int is_digit(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
-/* Function to calculate the length of a string */
-int str_len(const char *s) {
-    int length = 0;
-    while (s[length] != '\0') {
-        length++;
-    }
-    return length;
+/**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
 }
 
-/* Function to print an error message and exit */
-void print_error_and_exit() {
-    printf("Error\n");
-    exit(98);
+/**
+ * errors - handles errors for main
+ */
+void errors(void)
+{
+	printf("Error\n");
+	exit(98);
 }
 
-/* Function to multiply two large numbers */
-char *multiply_large_numbers(const char *num1, const char *num2) {
-    int len1 = str_len(num1);
-    int len2 = str_len(num2);
-    int i, j, carry, sum;
-    char *result;
+/**
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
-    if (len1 == 0 || len2 == 0) {
-        return NULL;
-    }
-
-    result = malloc(len1 + len2 + 1);
-    if (result == NULL) {
-        print_error_and_exit();
-    }
-
-    for (i = 0; i < len1 + len2; i++) {
-        result[i] = '0';
-    }
-    result[len1 + len2] = '\0';
-
-    for (i = len1 - 1; i >= 0; i--) {
-        if (!is_digit(num1[i])) {
-            free(result);
-            print_error_and_exit();
-        }
-        carry = 0;
-        for (j = len2 - 1; j >= 0; j--) {
-            if (!is_digit(num2[j])) {
-                free(result);
-                print_error_and_exit();
-            }
-            sum = (num1[i] - '0') * (num2[j] - '0') + (result[i + j + 1] - '0') + carry;
-            carry = sum / 10;
-            result[i + j + 1] = (sum % 10) + '0';
-        }
-        if (carry > 0) {
-            result[i + j + 1] = (result[i + j + 1] - '0' + carry) + '0';
-        }
-    }
-
-    /* Remove leading zeros */
-    for (i = 0; i < len1 + len2 - 1; i++) {
-        if (result[i] != '0') {
-            break;
-        }
-    }
-    if (i > 0) {
-        char *trimmed_result = malloc(len1 + len2 - i + 1);
-        if (trimmed_result == NULL) {
-            free(result);
-            print_error_and_exit();
-        }
-        for (j = 0; i < len1 + len2; j++, i++) {
-            trimmed_result[j] = result[i];
-        }
-        trimmed_result[j] = '\0';
-        free(result);
-        return trimmed_result;
-    }
-
-    return result;
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		errors();
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
+	{
+		digit1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		{
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
+		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
+	}
+	for (i = 0; i < len - 1; i++)
+	{
+		if (result[i])
+			a = 1;
+		if (a)
+			putchar(result[i] + '0');
+	}
+	if (!a)
+		putchar('0');
+	putchar('\n');
+	free(result);
+	return (0);
 }
-
-/* Main function */
-int main(int argc, char *argv[]) {
-    
-    char *result;
-
-    if (argc != 3) {
-        print_error_and_exit();
-    }
-
-    result = multiply_large_numbers(argv[1], argv[2]);
-    if (result == NULL) {
-        print_error_and_exit();
-    }
-
-    printf("%s\n", result);
-    free(result);
-    return 0;
-}
-
