b8613a7f99cfa82e45535b132d5e0578014cfff4
khayyam <khayyam.huseynli@gmail.com>
2024-03-20 02:15:58 +0400

    fixed betty style in op_functions

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -17,13 +17,13 @@ int op_add(int a, int b)
 }
 
 /**
-  2 * op_sub - function that subtracs two integers
-  3 *
-  4 * @a: integer
-  5 * @b: integer
-  6 *
-  7 * Return: returns the difference of a and b
-  8 */
+* op_sub - function that subtracs two integers
+*
+* @a: integer
+* @b: integer
+*
+* Return: returns the difference of a and b
+*/
 
 int op_sub(int a, int b)
 {
@@ -31,13 +31,13 @@ int op_sub(int a, int b)
 }
 
 /**
-  2 * op_mul - function that multiplicates two integers
-  3 *
-  4 * @a: integer
-  5 * @b: integer
-  6 *
-  7 * Return: returns the product of a and b
-  8 */
+* op_mul - function that multiplicates two integers
+*
+* @a: integer
+* @b: integer
+*
+* Return: returns the product of a and b
+*/
 
 int op_mul(int a, int b)
 {
@@ -45,13 +45,13 @@ int op_mul(int a, int b)
 }
 
 /**
-  2 * op_div - function that divides two integers
-  3 *
-  4 * @a: integer
-  5 * @b: integer
-  6 *
-  7 * Return: returns the division of a and b
-  8 */
+* op_div - function that divides two integers
+*
+* @a: integer
+* @b: integer
+*
+* Return: returns the division of a and b
+*/
 
 int op_div(int a, int b)
 {
@@ -59,13 +59,13 @@ int op_div(int a, int b)
 }
 
 /**
-  2 * op_mod - function that finds reminder of  two integers
-  3 *
-  4 * @a: integer
-  5 * @b: integer
-  6 *
-  7 * Return: returns the remainder of the division of a by b
-  8 */
+* op_mod - function that finds reminder of  two integers
+*
+* @a: integer
+* @b: integer
+*
+* Return: returns the remainder of the division of a by b
+*/
 
 int op_mod(int a, int b)
 {
