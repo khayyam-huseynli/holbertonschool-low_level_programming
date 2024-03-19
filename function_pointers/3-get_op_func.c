eee07542dfa33c24ca3f3e0b2c6cced3b55d1658
khayyam <khayyam.huseynli@gmail.com>
2024-03-20 02:12:07 +0400

    corrected while loop in get_op_func

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -23,11 +23,9 @@ int (*get_op_func(char *s))(int, int)
 
 	int i = 0;
 
-	while (i < 6)
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
 	{
-		if (*(ops[i].op) == *s)
-			return (ops[i].f);
 		i++;
 	}
-	return (ops[i - 1].f);
+	return (ops[i].f);
 }
