981bb0a6ff2bb12afc2798947e5ddcd3f72cbb85
khayyam <khayyam.huseynli@gmail.com>
2024-04-01 07:35:38 +0000

    handled malloc return case of null

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -13,6 +13,8 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new = (list_t *)malloc(sizeof(list_t));
 
+	if (new == NULL)
+		return (NULL);
 	new->str = strdup(str);
 	new->len = _strlen(str);
 	new->next = *head;
