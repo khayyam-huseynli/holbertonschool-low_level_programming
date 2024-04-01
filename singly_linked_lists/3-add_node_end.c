318bb1e9f650f5486ccf50574d81456faa16032b
khayyam <khayyam.huseynli@gmail.com>
2024-04-01 09:49:31 +0000

    added free

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -15,13 +15,17 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *new = (list_t *)malloc(sizeof(list_t));
 
 	if (new == NULL)
+	{
+		free(new);
 		return (NULL);
-
+	}
 	new->str = strdup(str);
-
 	if (new->str == NULL)
+	{
+		free(new->str);
+		free(new);
 		return (NULL);
-
+	}
 	new->len = _strlen(str);
 	if (p == NULL)
 	{
