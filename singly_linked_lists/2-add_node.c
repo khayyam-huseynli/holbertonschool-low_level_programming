bf3798a8ed9ac88f0fb5b2264642d299aa95f9bc
khayyam <khayyam.huseynli@gmail.com>
2024-04-01 07:22:07 +0000

    created file for adding element to linked list, used malloc

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a new node at the beginning
+ *            of a list_t list
+ * @head: head pointer to list_t list
+ * @str: string to be added
+ * Return: the address of the new element,
+ *         or NULL if it failed
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new = (list_t *)malloc(sizeof(list_t));
+
+	new->str = strdup(str);
+	new->len = strlen(str);
+	new->next = *head;
+	*head = new;
+
+	return (*head);
+}
