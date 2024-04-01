d38898d740e59918ca1f822fab740e79b40575b1
khayyam <khayyam.huseynli@gmail.com>
2024-04-01 12:07:51 +0000

    handled print nodes of dobly linked list

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the elements
+ *                  of a dlistint_t list
+ * @h: head pointer to dlistint_t list
+ * Return: number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	const list_t *p = h;
+	int count = 0;
+
+	while (p->next != NULL)
+	{
+		printf("%dn", p->n);
+		p = p->next;
+		count++;
+	}
+	return (count);
+}
