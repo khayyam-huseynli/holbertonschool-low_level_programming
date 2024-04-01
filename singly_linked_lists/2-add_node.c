1d0d5134dfdabb67a56a66ba382329c7e7234d43
khayyam <khayyam.huseynli@gmail.com>
2024-04-01 07:31:13 +0000

    added helper func _strlen

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -14,9 +14,29 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new = (list_t *)malloc(sizeof(list_t));
 
 	new->str = strdup(str);
-	new->len = strlen(str);
+	new->len = _strlen(str);
 	new->next = *head;
 	*head = new;
 
 	return (*head);
+}
+
+
+/**
+ * _strlen - function that returns the length of a string
+ * @s: string to be mesured
+ * Return: returns size of given string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		count++;
+	}
+
+	return (count);
 }
