579aebcc8d415f4e4dd47b61bb1e89c54c9f83b4
khayyam <khayyam.huseynli@gmail.com>
2024-04-01 07:52:21 +0000

    added to end

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+
+/**
+ * add_node_end - adds a new node at the beginning
+ *                of a list_t list
+ * @head: head pointer to list_t list
+ * @str: string to be added
+ * Return: the address of the new element,
+ *         or NULL if it failed
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *p = *head;
+	list_t *new = (list_t *)malloc(sizeof(list_t));
+
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+	new->len = _strlen(str);
+
+	while (p->next != NULL)
+		p = p->next;
+	new->next = NULL;
+	p->next = new;
+
+	return (p)
+}
+
+
+/**
+ * _strlen - function that returns the length of a string
+ * @s: string to be mesured
+ * Return: returns size of given string
+ */
+
+int _strlen(const char *s)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		count++;
+	}
+
+	return (count);
+}
