9165a24d6b7e74df1eec28f5bafa14493a3945d0
khayyam <khayyam.huseynli@gmail.com>
2024-04-01 06:21:59 +0000

    corrected betty style

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,3 +1,6 @@
+#ifndef LIST_H
+#define LIST_H
+
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
@@ -12,9 +15,11 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
+
+#endif
