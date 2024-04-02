7a6cf4965b4501c251248fd194f4664b21bbc12b
khayyam <khayyam.huseynli@gmail.com>
2024-04-02 10:32:53 +0000

    used while to iterate thourgh list elements to find last element

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - adds a new node at the end
+ *                    of a dlistint_t list
+ * @head: head pointer to list_t list
+ * @n: data to be added
+ * Return: the address of the new element,
+ *         or NULL if it failed
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *h = *head;
+	dlistint_t *new = (dlistint_t *)malloc(sizeof(dlistint_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->prev = h;
+	new->next = NULL;
+
+	if (h == NULL)
+		*head = new;
+	else
+	{
+		while (h != NULL)
+			h = h->next;
+		h->next = new;
+		new->prev = h;
+	}
+
+	return (new);
+}
