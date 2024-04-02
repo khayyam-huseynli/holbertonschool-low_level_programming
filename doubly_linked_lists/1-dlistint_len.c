202ec98fabe9cb3cb659e5b927f410238bdb03da
khayyam <khayyam.huseynli@gmail.com>
2024-04-02 04:39:45 +0000

    created func returns number of elements

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - preturns the number of elements in
+ *                a linked dlistint_t list
+ * @h: head pointer to list_t list
+ * Return: number of elements
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	const list_t *p = h;
+	int count = 0;
+
+	while (p != NULL)
+	{
+		p = p->next;
+		count++;
+	}
+	return (count);
+}
