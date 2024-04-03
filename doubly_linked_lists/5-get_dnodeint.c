5e3efbffde71f582a9f6b8173dbf10b41a23a4d8
khayyam <khayyam.huseynli@gmail.com>
2024-04-03 21:09:35 +0400

    putted if count=index inside while loop

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -16,14 +16,13 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 
 	if (h == NULL)
 		return (NULL);
-	if (h->next == NULL && index == 0)
-		return (h);
-	while (h->next != NULL)
+
+	while (h)
 	{
 		if (count == index)
 			return (h);
-		h = h->next;
 		count++;
+		h = h->next;
 	}
 	return (NULL);
 }
