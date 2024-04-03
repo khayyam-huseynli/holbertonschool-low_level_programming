a16ab396be4eba38e39fe5458a92a530adb2cec8
khayyam <khayyam.huseynli@gmail.com>
2024-04-03 04:34:29 +0000

    minor bug fixed

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -1,13 +1,13 @@
 #include "lists.h"
 
 /**
- * free_list - frees a list_t list
- * @head: head pointer to list_t list
+ * free_dlistint - frees a dlistint_t list
+ * @head: head pointer to dlistint_t list
  */
 
-void free_list(list_t *head)
+void free_dlistint(dlistint_t *head)
 {
-	list_t *tmp;
+	dlistint_t *tmp;
 
 	while (head != NULL)
 	{
