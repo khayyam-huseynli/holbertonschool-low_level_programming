2ca0ae3a6b5816efb7f6e9ace600ec72625589fb
root <khayyam.huseynli@gmail.com>
2024-04-05 07:31:25 +0000

    used open+read+write

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* read_textfile- Read text file print to STDOUT.
+* @filename: text file being read
+* @letters: number of letters to be read
+* Return: w- actual number of bytes read and printed
+* 0 when function fails or filename is NULL.
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buf;
+	ssize_t fd;
+	ssize_t wr;
+	ssize_t r;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * letters);
+
+	if (!buf)
+		return (0);
+
+	r = read(fd, buf, letters);
+
+	w = write(STDOUT_FILENO, buf, t);
+
+	if (w == -1)
+		return (0);
+
+	free(buf);
+	close(fd);
+	return (w);
+}
