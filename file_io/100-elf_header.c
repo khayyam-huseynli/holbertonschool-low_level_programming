3649b9c6fcc9c46141aecc40260b21adfb3daa8c
root <khayyam.huseynli@gmail.com>
2024-04-06 09:44:33 +0000

    handled spaces in print

--- file_io/100-elf_header.c
+++ file_io/100-elf_header.c
@@ -52,7 +52,7 @@ void print_magic(unsigned char *e_ident)
 {
 	int index;
 
-	printf(" Magic: ");
+	printf("  Magic:   ");
 	for (index = 0; index < EI_NIDENT; index++)
 	{
 		printf("%02x", e_ident[index]);
@@ -71,7 +71,7 @@ void print_magic(unsigned char *e_ident)
 
 void print_class(unsigned char *e_ident)
 {
-	printf(" Class: ");
+	printf("  Class                             ");
 
 	switch (e_ident[EI_CLASS])
 	{
@@ -96,7 +96,7 @@ void print_class(unsigned char *e_ident)
 
 void print_data(unsigned char *e_ident)
 {
-	printf(" Data: ");
+	printf("  Data                              ");
 
 	switch (e_ident[EI_DATA])
 	{
@@ -121,7 +121,7 @@ void print_data(unsigned char *e_ident)
 
 void print_version(unsigned char *e_ident)
 {
-	printf(" Version: %d",
+	printf("  Version:                           %d",
 	e_ident[EI_VERSION]);
 
 	switch (e_ident[EI_VERSION])
@@ -142,7 +142,7 @@ void print_version(unsigned char *e_ident)
 
 void print_osabi(unsigned char *e_ident)
 {
-	printf(" OS/ABI: ");
+	printf("  OS/ABI:                            ");
 
 	switch (e_ident[EI_OSABI])
 	{
@@ -188,7 +188,7 @@ void print_osabi(unsigned char *e_ident)
 
 void print_abi(unsigned char *e_ident)
 {
-	printf(" ABI Version: %d\n",
+	printf("  ABI Version:                       %d\n",
 	e_ident[EI_ABIVERSION]);
 }
 
@@ -202,7 +202,7 @@ void print_type(unsigned int e_type, unsigned char *e_ident)
 {
 	if (e_ident[EI_DATA] == ELFDATA2MSB)
 		e_type >>= 8;
-	printf(" Type: ");
+	printf("  Type:                              ");
 
 	switch (e_type)
 	{
@@ -234,7 +234,7 @@ void print_type(unsigned int e_type, unsigned char *e_ident)
 
 void print_entry(unsigned long int e_entry, unsigned char *e_ident)
 {
-	printf(" Entry point address: ");
+	printf("  Entry point address:               ");
 
 	if (e_ident[EI_DATA] == ELFDATA2MSB)
 	{
